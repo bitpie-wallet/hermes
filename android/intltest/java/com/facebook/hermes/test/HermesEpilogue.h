ec228d2cccbc7d480c0e5f2edd81c8f309ae750c
xumoyan <13381205621@163.cm>
2024-12-15 10:01:39 +0800

    modify path

--- android/intltest/java/com/facebook/hermes/test/HermesEpilogue.h
+++ android/intltest/java/com/facebook/hermes/test/HermesEpilogue.h
@@ -20,7 +20,7 @@ namespace jni = ::facebook::jni;
 class HermesEpilogue : public jni::HybridClass<HermesEpilogue> {
  public:
   constexpr static auto kJavaDescriptor =
-      "Lcom/bitpie/hermes/HermesEpilogue;";
+      "Lcom/txdecode/hermes/HermesEpilogue;";
   static jni::local_ref<jbyteArray> getHermesBytecodeMetadata(
       jni::alias_ref<jclass>,
       jni::alias_ref<jbyteArray> bytes);
